1b3944bfa1573fecc87ed2d9bfed113b74918c80
Aleksandar Jovanov <aleksandar.jovanov.1995@gmail.com>
2020-01-08 11:42:51 +0100

    advanced tests for serialization code

--- test/serialization/test_serialization.cpp
+++ test/serialization/test_serialization.cpp
@@ -2,7 +2,7 @@
 
 #include<random>
 
-int main(int argc, char** argv) {
+void basic_test() {
     std::default_random_engine generator;
     generator.seed(1337);
 
@@ -24,6 +24,84 @@ int main(int argc, char** argv) {
     std::cout << "Network 2" << std::endl;
     std::cout << net2;
     std::cout << "These two should be identical" << std::endl;
-    
+}
+
+int test(int argc, char** argv) {
+    std::uint32_t n_input  = std::stoi(argv[1]);
+    std::uint32_t n_hidden = std::stoi(argv[2]);
+    std::uint32_t n_output = std::stoi(argv[3]);
+
+    std::uint32_t ng_func_i = std::stoi(argv[4]);
+    std::uint32_t w_func_i  = std::stoi(argv[5]);
+
+    std::uint32_t expect_crash = std::stoi(argv[6]);
+
+    double p = 0.5;
+    double wa = 0.0;
+    double wb = 1.0;
+
+    int seed = 1337;
+    std::default_random_engine generator;
+    generator.seed(seed);
+
+    NetworkGeneratorFunction rci;
+
+    try {
+        rci = random_connections_init(generator, p);
+    } catch (std::invalid_argument&) {
+        if (expect_crash)
+            return 0;
+        
+        return -1;    
+    }
+
+    std::array<NetworkGeneratorFunction, 4> ng_funcs = {
+        fully_connected_init(),
+        perceptron_init_simple(),
+        perceptron_init_hidden(),
+        rci
+    };
+
+    std::array<WeightInitializerFunction, 3> wei_funcs = {
+        uniform_weights(generator, wa, wb), 
+        normal_weights(generator, wa, wb), 
+        glorot_weights(generator)
+    };
+
+    try {
+        Network net = Network(n_input, n_hidden, n_output, 
+                              ng_funcs[ng_func_i], wei_funcs[w_func_i]);
+
+        std::string the_path = std::string("/tmp/test_serialization_"); 
+        for(uint32_t i = 1; i < 7; i++)
+            the_path += std::string(argv[i]) + std::string("_");
+        the_path += std::string(".cereal");
+
+        net.save(the_path);
+
+        Network net2(the_path);
+
+        std::system((std::string("du -h ") + the_path).c_str());
+    } catch (std::invalid_argument&) {
+        if (expect_crash)
+            return 0;
+        
+        return -1;
+    } catch (std::logic_error&) {
+        if (expect_crash)
+            return 0;
+        
+        return -2;
+    }
+
     return 0;
+}
+
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        basic_test();
+        return 0;
+    }
+
+    return test(argc, argv);
 }
